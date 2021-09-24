1bf2cda5a6907665df1e7c9dc30224e3de6baf28
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2021-09-24 14:44:58 -0300

    arrumando envio de dados para api

--- Controller/PairProgrammingController.php
+++ Controller/PairProgrammingController.php
@@ -22,20 +22,9 @@ class PairProgrammingController extends BaseController
         $assignee = $this->request->getStringParam('assignee');
         $name = $this->request->getStringParam('name');
         $task = $this->request->getStringParam('task');
-        $gestaosistemasApi = $this->configModel->getOption('gestaosistemas_api');
 
-        $curl = curl_init($gestaosistemasApi);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($curl, CURLOPT_POSTFIELDS , [
-            'name' => $name,
-            'assignee' => $assignee,
-            'activity' => 'Pareamento',
-            'task' => $task,
-        ]);
-
-        curl_exec($curl);
-        curl_close($curl);
+        $this->sendToApi($assignee, $task);        
+        $this->sendToApi($name, $task);        
         
         $duplicate = $this->pairProgrammingModel->findByTaskAndName($task, $name);
         if (empty($duplicate)) {
@@ -44,6 +33,23 @@ class PairProgrammingController extends BaseController
         $this->response->redirect($this->helper->url->to('TaskViewController', 'show', array('task_id' => $task)), true);
     }
     
+    private function sendToApi($name, $task)
+    {
+        $gestaosistemasApi = $this->configModel->getOption('gestaosistemas_api');
+
+        $curl = curl_init($gestaosistemasApi);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl, CURLOPT_POSTFIELDS , [
+            'name' => $name,
+            'activity' => 'Pareamento',
+            'task' => $task,
+        ]);
+
+        curl_exec($curl);
+        curl_close($curl);
+    }
+
     public function remove()
     {
         $id = $this->request->getStringParam('pair_programming_id');
